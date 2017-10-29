a7cc0c94b9750096484723caed2b0cbe1619867c
Unknown <alucas44@academic.rrc.ca>
2017-10-29 17:01:28 -0500

    Part 2 Done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,8 @@ class PagesController < ApplicationController
 
   def about
   end
+
+  def lizard
+    @lizard = params[:number].to_i
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
 
   get 'pages', to: 'pages#home', as: 'pages'
 
-  get 'pages/about_us', to: 'pages#about', as: 'about'
+  get 'pages/about', to: 'pages#about', as: 'about_us'
+
+  get 'lizard/:number', to: 'pages#lizard', as: 'lizards'
 end
